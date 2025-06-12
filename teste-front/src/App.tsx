78c0548b550b0f860ba6992e1249ed99275b141d
Lucas Tropardi <lucastropardi@hotmail.com>
2025-06-12 07:06:48 -0300

    feat: cria projeto react com typescript, integra tailwind css e flowbite

---
+++ teste-front/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="">
+        <h1 className="text-3xl font-bold text-blue-700">Tailwind funcionando!</h1>
+      </div>  
+    </>
+  )
+}
+
+export default App
